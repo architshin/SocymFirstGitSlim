7782924c0c2b0a70bc8a2279afee19f224a10797
Shinzo SAITO <architshin@websarva.com>
2019-10-15 15:51:41 +0900

    プロジェクトの作成、及び、初期処理コードの追加。

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+use Slim\Factory\AppFactory;
+
+require_once($_SERVER["DOCUMENT_ROOT"]."/socymfirstgitslim/vendor/autoload.php");
+
+require_once($_SERVER["DOCUMENT_ROOT"]."/socymfirstgitslim/containerSetups.php");
+$app = AppFactory::create();
+require_once($_SERVER["DOCUMENT_ROOT"]."/socymfirstgitslim/routes.php");
+$app->run();
