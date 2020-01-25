617aec32f17749339ac33749a4a32dd7e4529d5a
Shinzo SAITO <architshin@websarva.com>
2020-01-25 16:37:25 +0900

    本脱稿9章に合わせて変更。

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 use Slim\Factory\AppFactory;
 
-require_once($_SERVER["DOCUMENT_ROOT"]."/../vendor/autoload.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/socymfirstgitslim/vendor/autoload.php");
 
-require_once($_SERVER["DOCUMENT_ROOT"]."/../containerSetups.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/socymfirstgitslim/containerSetups.php");
 $app = AppFactory::create();
-require_once($_SERVER["DOCUMENT_ROOT"]."/../routes.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/socymfirstgitslim/routes.php");
 $app->run();
--- routes.php
+++ routes.php
@@ -1,5 +1,6 @@
 <?php
 use SocymSlim\FirstGitSlim\controllers\HelloController;
 
+$app->setBasePath("/socymfirstgitslim/public");
 $app->any("/sayHello", HelloController::class.":sayHello");
 $app->any("/sayHelloWithTemp", HelloController::class.":helloWithTemplate");
