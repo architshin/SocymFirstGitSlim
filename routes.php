269bc79185a3c6f1859ede49a0252ca720985440
Shinzo SAITO <architshin@websarva.com>
2020-01-26 02:27:45 +0900

    ドキュメントルートをpublicに変更。

--- routes.php
+++ routes.php
@@ -1,6 +1,5 @@
 <?php
 use SocymSlim\FirstGitSlim\controllers\HelloController;
 
-$app->setBasePath("/socymfirstgitslim/public");
 $app->any("/sayHello", HelloController::class.":sayHello");
 $app->any("/sayHelloWithTemp", HelloController::class.":helloWithTemplate");
